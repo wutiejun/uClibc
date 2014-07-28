5184027913e7f909f5c915836909bcc92c9e196e
Claudiu Zissulescu <claziss@synopsys.com>
2014-07-28 09:15:30 +0200

    Hacks needed to run all the tls tests correctly on the ARC VP

--- libc/sysdeps/linux/arc/syscall.c
+++ libc/sysdeps/linux/arc/syscall.c
@@ -9,9 +9,39 @@
 #include <sys/types.h>
 #include <sys/syscall.h>
 
+#ifdef __HS__
+#include <stdarg.h>
+extern long syscall (long int sysnum, ...);
+
+long arc_wrapper_syscall (long int sysnum, long a, long b, long c, long d, long e, long f)
+{
+  return INLINE_SYSCALL_NCS(sysnum, 6, a, b, c, d, e, f);
+}
+
+long syscall (long int sysnum, ...)
+{
+  va_list ap;
+  long a, b, c, d, e, f;
+
+  va_start (ap, sysnum);
+  a = va_arg (ap, long);
+  b = va_arg (ap, long);
+  c = va_arg (ap, long);
+  d = va_arg (ap, long);
+  e = va_arg (ap, long);
+  f = va_arg (ap, long);
+  va_end(ap);
+
+  return arc_wrapper_syscall(sysnum, a, b, c, d, e, f);
+}
+
+#else
+
 extern long syscall(long int sysnum, long a, long b, long c, long d, long e, long f);
 
 long syscall(long int sysnum, long a, long b, long c, long d, long e, long f)
 {
 	return INLINE_SYSCALL_NCS(sysnum, 6, a, b, c, d, e, f);
 }
+
+#endif
